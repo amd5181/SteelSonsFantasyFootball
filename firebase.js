6ff09be68828d68b914c2ee1ea5bc3be3601422c
amd5181 <adavidfr2006@gmail.com>
2025-06-22 11:08:44 -0400

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,13 +1,15 @@
-// firebase.js  – put at repo root
+// firebase.js
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyCyJ4crvTcbE2uHAfZHiTICcGP74kKFWvE",
+  authDomain: "steelsonsfantasyfootball.firebaseapp.com",
+  projectId: "steelsonsfantasyfootball",
+  storageBucket: "steelsonsfantasyfootball.appspot.com",
+  messagingSenderId: "693967635959",
+  appId: "1:693967635959:web:1433bf20c411bbf5cb5af2",
+  measurementId: "G-HB0N5WMBFM"
 };
 
 const app = initializeApp(firebaseConfig);
