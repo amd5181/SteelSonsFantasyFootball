c0f3d476d2b73a48d6a66753644d3d0d3b239e0f
amd5181 <adavidfr2006@gmail.com>
2025-06-22 18:34:59 +0000

    Point Firebase Storage to correct bucket

--- firebase.js
+++ firebase.js
@@ -1,4 +1,4 @@
-// firebase.js
+// lib/firebase.js
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
@@ -7,12 +7,13 @@ const firebaseConfig = {
   apiKey: "AIzaSyCyJ4crvTcbE2uHAfZHiTICcGP74kKFWvE",
   authDomain: "steelsonsfantasyfootball.firebaseapp.com",
   projectId: "steelsonsfantasyfootball",
-  storageBucket: "steelsonsfantasyfootball.firebasestorage.app",
-  messagingSenderId: "693967635959",
-  appId: "1:693967635959:web:1433bf20c411bbf5cb5af2",
-  measurementId: "G-HB0N5WMBFM"
+  // leave storageBucket out; we’ll pass it explicitly below
 };
 
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
-export const storage = getStorage(app); // <-- this line was missing
+
+export const db      = getFirestore(app);
+export const storage = getStorage(
+  app,
+  "gs://steelsonsfantasyfootball.firebasestorage.app"   // ← your real bucket
+);
