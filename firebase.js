17f6a6314ad84680b49734402e7ae75c842fb683
amd5181 <adavidfr2006@gmail.com>
2025-06-22 18:30:25 +0000

    Fix: add Firebase Storage initialization

--- firebase.js
+++ firebase.js
@@ -1,12 +1,13 @@
 // firebase.js
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCyJ4crvTcbE2uHAfZHiTICcGP74kKFWvE",
   authDomain: "steelsonsfantasyfootball.firebaseapp.com",
   projectId: "steelsonsfantasyfootball",
-  storageBucket: "steelsonsfantasyfootball.appspot.com",
+  storageBucket: "steelsonsfantasyfootball.firebasestorage.app",
   messagingSenderId: "693967635959",
   appId: "1:693967635959:web:1433bf20c411bbf5cb5af2",
   measurementId: "G-HB0N5WMBFM"
@@ -14,3 +15,4 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+export const storage = getStorage(app); // <-- this line was missing
